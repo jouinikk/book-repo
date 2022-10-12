efe0566fb1efc404a60a7fe6c4ff9415f36d94ac
unknown <jouinikk1@gmail.com>
2022-10-12 17:29:03 +0100

    deleted ex2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { Ex2Component } from './ex2/ex2.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    Ex2Component,
-    
-  ],
+    ],
   imports: [
     BrowserModule,
   ],
